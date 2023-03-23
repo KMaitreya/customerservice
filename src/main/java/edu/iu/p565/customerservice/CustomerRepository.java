1a8c62aa914af2d2c4036c86af76b8c0d4e26a2d
Maitreya Kanitkar <maitreya.kanitkar@gmail.com>
2023-03-22 23:46:55 -0400

    Updated 3 java files

---
+++ src/main/java/edu/iu/p565/customerservice/CustomerRepository.java
@@ -0,0 +1,13 @@
+package edu.iu.p565.customerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerRepository {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerRepository.class, args);
+	}
+
+}
